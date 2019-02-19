b57627ec77c9052d23cf88f6527dca449d0d3741
suranat <suranat2001@gmail.com>
2019-02-19 19:34:47 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+var response=require('./sample.js');
+console.log(response.add(3,5));
+console.log(response.pi);
---
+++ sample.js
@@ -0,0 +1,12 @@
+console.log('Hello World');
+
+var pi=3.14;
+
+var add=function (a,b) {
+   return a+b;
+}
+
+var obj={};
+
+exports.pi=pi;
+exports.add=add;
